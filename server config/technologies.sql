13a647287258119263fcaf911d2c18724fb2fc07
mufti ismi rizqi <muftiismirizqi@gmail.com>
2017-09-19 06:35:38 +0700

    success crud ionic with php mysql

---
+++ server config/technologies.sql
@@ -0,0 +1,63 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.1
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: 19 Sep 2017 pada 01.34
+-- Versi Server: 10.1.16-MariaDB
+-- PHP Version: 5.6.24
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `technologies`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `technologies`
+--
+
+CREATE TABLE `technologies` (
+  `id` int(3) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `description` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `technologies`
+--
+
+INSERT INTO `technologies` (`id`, `name`, `description`) VALUES
+(1, 'Javascript', 'javascript is the best technology now because you can create any apps for any platform with javascript hmmm seeems legit');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `technologies`
+--
+ALTER TABLE `technologies`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `technologies`
+--
+ALTER TABLE `technologies`
+  MODIFY `id` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
